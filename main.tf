cf65e8322d4b0d9efcb4932e91359f708a919b3d
ingdamiangonzalez <ing.damian.gonzalez@gmail.com>
2023-06-20 19:20:50 -0300

    add checkov aws test

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-# resource "aws_instance" "foo" {
-#   ami           = "ami-0ff8a91507f77f867"
-#   instance_type = "t3.4xlarge" # invalid type!
-# }
+resource "aws_instance" "foo" {
+  ami           = "ami-0ff8a91507f77f867"
+  instance_type = "t3.large" # invalid type!
+}
